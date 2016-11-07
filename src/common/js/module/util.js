4069239dde0bc3addace9967227be75ad2c1803d
Sun Weiguang <sunweiguang224@gmail.com>
2016-11-07 14:30:42 +0800

    no message

--- src/common/js/module/util.js
+++ src/common/js/module/util.js
@@ -1,10 +1,27 @@
 module.exports = {
-  // 生成随机小数
-  randomFloat: function(min, max) {
+  // 生成随机数
+  random: function(min, max) {
     return Math.random() * (max - min) + min;
   },
-  // 生成随机整数
-  randomInt: function(min, max) {
-    return Math.floor(swg.randomFloat(min, max));
+	/**
+	 *
+	 * @param areaArr
+		 var areaArr = [
+			 {
+				 min: -1,
+				 max: 1
+			 },{
+						min: -1,
+						max: 1
+					},
+	 		];
+	 */
+  random2: function(areaArr) {
+		Math.max(1,2,3)
+		for(var i in areaArr){
+			var area = areaArr[i];
+			Math.random() * (max - min) + min;
+		}
+    return
   },
 };
