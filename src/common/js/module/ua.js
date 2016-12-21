ee0e7091aae54baad895085d1d80f6346437a445
Sun Weiguang <sunweiguang224@gmail.com>
2016-12-21 15:02:04 +0800

    no message

--- src/common/js/module/ua.js
+++ src/common/js/module/ua.js
@@ -6,17 +6,21 @@ function Ua(){
 	this.ua = navigator.userAgent;
 	this.cache = {};
 }
+
 Ua.prototype = {
-	match: function(regexStr){
-		return new RegExp(regexStr, 'g').exec(this.ua) ? true : false;
+	match: function(regex){
+		if(Object.prototype.toString.call(regex) === '[object String]'){
+			regex = new RegExp(regex, 'g');
+		}
+		return regex.test(this.ua);
 	},
-	getFromCache: function(cacheName, regStrArray){
+	getFromCache: function(cacheName, regexArray){
 		// 如果未缓存过cache[cacheName]，则缓存对正则数组regStrArray匹配的结果
 		if(this.cache[cacheName] === undefined){
 			// 正则数组regStrArray匹配的结果
 			var cacheValue;
-			for(var i in regStrArray){
-				cacheValue = this.match(regStrArray[i]);
+			for(var i in regexArray){
+				cacheValue = this.match(regexArray[i]);
 				if(cacheValue){
 					break;
 				}
@@ -34,7 +38,7 @@ Ua.prototype = {
 	 * ios		Mozilla/5.0 (iPod; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/51.0.2704.64 Mobile/13F69 Safari/601.1.46
 	 */
 	isChrome: function(){
-		return this.getFromCache(arguments.callee.name, ['Chrome', 'CriOS']);
+		return this.getFromCache('isChrome', ['Chrome', 'CriOS']);
 	},
 	/**
 	 * uc浏览器
@@ -43,7 +47,7 @@ Ua.prototype = {
 	 * ios		Mozilla/5.0 (iPod touch; CPU iPhone OS 9_3_2 like Mac OS X; zh-CN) AppleWebKit/537.51.1 (KHTML, like Gecko) Mobile/13F69 UCBrowser/10.9.16.802 Mobile
 	 */
 	isUc: function(){
-		return this.getFromCache(arguments.callee.name, ['UCBrowser']);
+		return this.getFromCache('isUc', ['UCBrowser']);
 	},
 	/**
 	 * QQ浏览器
@@ -52,7 +56,7 @@ Ua.prototype = {
 	 * ios		Mozilla/5.0 (iPod touch; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/6.0 MQQBrowser/6.7.2 Mobile/13F69 Safari/8536.25 MttCustomUA/2
 	 */
 	isQqBrowser: function(){
-		return this.getFromCache(arguments.callee.name, ['MQQBrowser']);
+		return this.getFromCache('isQqBrowser', ['MQQBrowser']);
 	},
 	/**
 	 * 小米系统浏览器
@@ -60,7 +64,7 @@ Ua.prototype = {
 	 * android	Mozilla/5.0 (Linux; U; Android 6.0; zh-cn; MI 5 Build/MRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/46.0.2490.85 Mobile Safari/537.36 XiaoMi/MiuiBrowser/2.1.1
 	 */
 	isXiaoMiBrowser: function(){
-		return this.getFromCache(arguments.callee.name, ['XiaoMi/MiuiBrowser']);
+		return this.getFromCache('isXiaoMiBrowser', ['XiaoMi/MiuiBrowser']);
 	},
 	/**
 	 * safari浏览器
@@ -68,7 +72,25 @@ Ua.prototype = {
 	 * ios		Mozilla/5.0 (iPod touch; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1
 	 */
 	isSafari: function(){
-		return this.getFromCache(arguments.callee.name, ['Version/\\d?[.]\\d Mobile/.* Safari/.*']);
+		return this.getFromCache('isSafari', ['Version/\\d?[.]\\d Mobile/.* Safari/.*']);
+	},
+	/**
+	 * 百度浏览器
+	 * @returns {true | false}
+	 * android	Mozilla/5.0 (Linux; Android 6.0; MI 5 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/48.0.2564.116 Mobile Safari/537.36 baidubrowser/7.8.12.0 (Baidu; P1 6.0)
+	 * ios
+	 */
+	isBaiduBrowser: function(){
+		return this.getFromCache('isBaiduBrowser', ['baidubrowser']);
+	},
+	/**
+	 * 搜狗浏览器
+	 * @returns {true | false}
+	 * android	Mozilla/5.0 (Linux; Android 6.0; MI 5; Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/46.0.2490.92 SDK/1.2.2.675 Mobile Safari/537.36 SogouMSE,SogouMobileBrowser/5.5.0
+	 * ios
+	 */
+	isSougouBrowser: function(){
+		return this.getFromCache('isSougouBrowser', ['SogouMobileBrowser']);
 	},
 	/**
 	 * 新浪微博
@@ -77,7 +99,7 @@ Ua.prototype = {
 	 * ios		Mozilla/5.0 (iPod touch; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 Weibo (iPod5,1__weibo__5.4.0__iphone__os9.3.2)
 	 */
 	isSinaWeiBo: function(){
-		return this.getFromCache(arguments.callee.name, ['Weibo']);
+		return this.getFromCache('isSinaWeiBo', ['Weibo']);
 	},
 	/**
 	 * QQ+QQ空间（QQ和QQ空间ua相同）
@@ -86,7 +108,7 @@ Ua.prototype = {
 	 * ios		Mozilla/5.0 (iPod touch; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 QQ/6.3.5.437 V1_IPH_SQ_6.3.5_1_APP_A Pixel/640 Core/UIWebView NetType/WIFI Mem/12
 	 */
 	isQq: function(){
-		return this.getFromCache(arguments.callee.name, ['QQ']);
+		return this.getFromCache('isQq', [/QQ\/[0-9]?/]);
 	},
 	/**
 	 * 微信+朋友圈（微信和朋友圈ua相同）
@@ -95,8 +117,7 @@ Ua.prototype = {
 	 * ios		Mozilla/5.0 (iPod touch; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 MicroMessenger/6.3.19 NetType/WIFI Language/zh_CN
 	 */
 	isWeiXin: function(){
-		return match('Weibo');
-		return this.getFromCache(arguments.callee.name, ['MQQBrowser']);
+		return this.getFromCache('isWeiXin', ['MicroMessenger']);
 	},
 	/**
 	 * 支付宝
@@ -105,8 +126,7 @@ Ua.prototype = {
 	 * ios		Mozilla/5.0 (iPhone; CPU iPhone OS 9_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13E233 ChannelId(12) Nebula PSDType(1) AlipayDefined(nt:WIFI,ws:320|504|2.0) AliApp(AP/9.6.6.05070802) AlipayClient/9.6.6.05070802 Language/zh-Hans
 	 */
 	isAlipay: function(){
-		return match('AlipayClient');
-		return this.getFromCache(arguments.callee.name, ['AlipayClient']);
+		return this.getFromCache('isAlipay', ['AlipayClient']);
 	},
 	/**
 	 * 搜狐新闻客户端
@@ -115,21 +135,21 @@ Ua.prototype = {
 	 * ios		Mozilla/5.0 (iPhone; CPU iPhone OS 9_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13E233 JsKit/1.0 (iOS)
 	 */
 	isSohuNewsClient: function(){
-		return this.getFromCache(arguments.callee.name, ['SohuNews', 'JsKit']);
+		return this.getFromCache('isSohuNewsClient', ['SohuNews', 'JsKit']);
 	},
 	/**
 	 * android
 	 * @returns {true | false}
 	 */
 	isAndroid: function(){
-		return this.getFromCache(arguments.callee.name, ['Android']);
+		return this.getFromCache('isAndroid', ['Android']);
 	},
 	/**
 	 * ios
 	 * @returns {true | false}
 	 */
 	isIos: function(){
-		return this.getFromCache(arguments.callee.name, ['iPhone']);
+		return this.getFromCache('isIos', ['iPhone']);
 	},
 };
 Ua.prototype.constructor = Ua;
