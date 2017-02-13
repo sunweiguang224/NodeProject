4d29d2584fd42b3964d7947dbfcead619e0d9dde
sunweiguang <sunweiguang224@gmail.com>
2017-02-13 14:03:29 +0800

    update

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
         var filePath = filePaths[i];
         alias[path.basename(filePath).replace('.js', '')] = filePath;
       }
+      console.log('已加载webpack alias:');
+      console.log(alias);
       return alias;
     })()
   },
